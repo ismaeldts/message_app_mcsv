07bf3c65c1feb8f919e9d97d0d367fc0f4b4b91e
ismaeldts <ismaeltrochasarmiento@gmail.com>
2024-10-03 23:33:58 -0500

    Aprendiendo a utilizar Websockets

---
+++ src/main/java/com/lms/mcsv_chat/McsvChatApplication.java
@@ -0,0 +1,13 @@
+package com.lms.mcsv_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvChatApplication.class, args);
+	}
+
+}
